f4389637a4cd6b90d9fc496810071ac458174535
martymcquaid <martymcquaid@users.noreply.github.com>
2025-12-01 16:08:59 +0000

    AI edit: /root/ChitterChat/team-call-connect/TwilioAPIService/chipify_projects/6b78796f-4a8b-42d0-8f41-1110fdcfe30d/src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,44 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { CartProvider } from './contexts/CartContext'
+import Header from './components/Header'
+import Footer from './components/Footer'
 import Home from './pages/Home'
+import CategoryPage from './pages/CategoryPage'
+import ProductPage from './pages/ProductPage'
+import CartPage from './pages/CartPage'
+import CheckoutPage from './pages/CheckoutPage'
+import AccountPage from './pages/AccountPage'
+import AboutPage from './pages/AboutPage'
+import ContactPage from './pages/ContactPage'
+import FAQPage from './pages/FAQPage'
+import BlogPage from './pages/BlogPage'
+import SearchPage from './pages/SearchPage'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <CartProvider>
+      <BrowserRouter>
+        <div className="min-h-screen flex flex-col">
+          <Header />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/category/:categoryId" element={<CategoryPage />} />
+              <Route path="/product/:productId" element={<ProductPage />} />
+              <Route path="/cart" element={<CartPage />} />
+              <Route path="/checkout" element={<CheckoutPage />} />
+              <Route path="/account" element={<AccountPage />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/contact" element={<ContactPage />} />
+              <Route path="/faq" element={<FAQPage />} />
+              <Route path="/blog" element={<BlogPage />} />
+              <Route path="/search" element={<SearchPage />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </CartProvider>
   )
 }
 
